a6312a87ca42f67b2aa28717a0c42508211e13b4
Riadh Khedhiri <riadh.khedhiri@gmail.com>
2021-10-22 13:50:42 -0400

    Implement Nav component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
@@ -12,6 +12,7 @@ export default function Home() {
       </Head>
 
       <Header/>
+      <Nav />
     </div>
   )
 }
