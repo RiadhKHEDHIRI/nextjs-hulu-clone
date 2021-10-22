1f714fef7f8362caa1f6bcb39acc8992021534b7
Riadh Khedhiri <riadh.khedhiri@gmail.com>
2021-10-22 17:30:01 -0400

    Implement Results list

--- utils/requests.js
+++ utils/requests.js
@@ -1,49 +1,49 @@
-const IMDB_API_KEY = process.env.IMDB_API_KEY;
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 const endpoints = {
   fetchTrending: {
     title: 'Trending',
-    url: `/trending/all/week?language=en-US&api_key=${IMDB_API_KEY}`
+    url: `/trending/all/week?language=en-US&api_key=${TMDB_API_KEY}`
   },
   fetchTopRated: {
     title: 'Top Rated',
-    url:`/movie/top_rated?language=en-US&api_key=${IMDB_API_KEY}`,
+    url:`/movie/top_rated?language=en-US&api_key=${TMDB_API_KEY}`,
   },
   fetchActionMovies: {
     title: 'Action',
-    url:`/discover/movie?api_key=${IMDB_API_KEY}&with_genres=28`,
+    url:`/discover/movie?api_key=${TMDB_API_KEY}&with_genres=28`,
   },
   fetchComedyMovies: {
     title: 'Comedy',
-    url:`/discover/movie?api_key=${IMDB_API_KEY}&with_genres=35`,
+    url:`/discover/movie?api_key=${TMDB_API_KEY}&with_genres=35`,
   },
   fetchHorrorMovies: {
     title: 'Horror',
-    url:`/discover/movie?api_key=${IMDB_API_KEY}&with_genres=27`,
+    url:`/discover/movie?api_key=${TMDB_API_KEY}&with_genres=27`,
   },
   fetchRomanceMovies: {
     title: 'Romance',
-    url:`/discover/movie?api_key=${IMDB_API_KEY}&with_genres=10749`,
+    url:`/discover/movie?api_key=${TMDB_API_KEY}&with_genres=10749`,
   },
   fetchMystery: {
     title: 'Action',
-    url:`/discover/movie?api_key=${IMDB_API_KEY}&with_genres=9648`,
+    url:`/discover/movie?api_key=${TMDB_API_KEY}&with_genres=9648`,
   },
   fetchSciFi: {
     title: 'Sci-Fi',
-    url:`/discover/movie?api_key=${IMDB_API_KEY}&with_genres=878`,
+    url:`/discover/movie?api_key=${TMDB_API_KEY}&with_genres=878`,
   },
   fetchWestern: {
     title: 'Western',
-    url:`/discover/movie?api_key=${IMDB_API_KEY}&with_genres=37`,
+    url:`/discover/movie?api_key=${TMDB_API_KEY}&with_genres=37`,
   },
   fetchAnimation: {
     title: 'Animation',
-    url:`/discover/movie?api_key=${IMDB_API_KEY}&with_genres=16`,
+    url:`/discover/movie?api_key=${TMDB_API_KEY}&with_genres=16`,
   },
   fetchTV: {
     title: 'TV Movies',
-    url:`/discover/movie?api_key=${IMDB_API_KEY}&with_genres=10770`,
+    url:`/discover/movie?api_key=${TMDB_API_KEY}&with_genres=10770`,
   },
 }
 
